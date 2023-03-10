3ad7d57f307fe7926d9b36e039f75438488581a3
omrajindra <omrajindra.ori@gmail.com>
2023-03-10 17:33:23 +0545

    Dark Mode Enable on About Page

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,32 +2,71 @@ import React, { useState } from "react";
 
 export default function TextForm(props) {
   const handleUpClick = () => {
-    console.log("UpperCase was Clicked" + text);
+    //console.log("UpperCase was Clicked" + text);
     let newText = text.toUpperCase();
     setText(newText);
   };
 
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+  };
+
+  const handleCapitalizedClick = () => {
+    for (let i = 0; i < text.length; i++) {
+      let newText = text[i].charAt(0).toUpperCase() + text[i].slice(1);
+      setText(newText);
+    }
+  };
+
   const handleOnChange = (event) => {
-    console.log("On Change");
+    //console.log("On Change");
     setText(event.target.value);
   };
-  const [text, setText] = useState("Enter Your Text Here");
+  const [text, setText] = useState("");
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="myBox"
-          rows="8"
-          value={text}
-          onChange={handleOnChange}
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          Clear Text
+        </button>
+        <button
+          className="btn btn-primary mx-2"
+          onClick={handleCapitalizedClick}
+        >
+          Capitalized First Word
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        {" "}
-        Convert to Uppercase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h1>Your Text Summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
